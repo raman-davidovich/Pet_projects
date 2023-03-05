9dc9ff08cd9afe8d75105624916b37d1ae3067bc
Raman Davidovich <raman.davidovich@gmail.com>
2023-03-05 11:59:42 +0300

    feat: created Header component JSX

---
+++ react_portfolio_website/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com/in/raman-davidovich" target="_blank"></a>
+      <a href="https://github.com/raman-davidovich" target="_blank"></a>
+      <a href="https://dribbble.com" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
