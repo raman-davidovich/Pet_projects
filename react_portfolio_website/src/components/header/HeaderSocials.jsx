f139ce01bd716bebaf0ccdb8719ef6934f49862b
Raman Davidovich <raman.davidovich@gmail.com>
2023-03-05 12:10:36 +0300

    feat: added HeaderSocials component

--- react_portfolio_website/src/components/header/HeaderSocials.jsx
+++ react_portfolio_website/src/components/header/HeaderSocials.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com/in/raman-davidovich" target="_blank"></a>
-      <a href="https://github.com/raman-davidovich" target="_blank"></a>
-      <a href="https://dribbble.com" target="_blank"></a>
+      <a href="https://linkedin.com/in/raman-davidovich" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/raman-davidovich" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://dribbble.com" target="_blank">
+        <FiDribbble />
+      </a>
     </div>
   );
 };
