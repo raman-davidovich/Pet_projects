0ea935f3ca735b1e4002e6ab70112adf1d254f73
Raman Davidovich <raman.davidovich@gmail.com>
2023-03-12 20:58:32 +0300

    refactor: added fake reviews in Testimonials

--- react_portfolio_website/src/components/testimonials/Testimonials.jsx
+++ react_portfolio_website/src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./testimonials.css";
-import AVTR1 from "../../assets/avatar1.jpg";
-import AVTR2 from "../../assets/avatar2.jpg";
-import AVTR3 from "../../assets/avatar3.jpg";
-import AVTR4 from "../../assets/avatar4.jpg";
+import AVTR1 from "../../assets/avatar1.webp";
+import AVTR2 from "../../assets/avatar2.jpeg";
+import AVTR3 from "../../assets/avatar3.webp";
+import AVTR4 from "../../assets/avatar4.jpeg";
 
 // import Swiper core and required modules
 import { Pagination } from "swiper";
@@ -16,27 +16,25 @@ import "swiper/css/pagination";
 const data = [
   {
     avatar: AVTR1,
-    name: "Tina Snow",
-    review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at commodi vel nam doloremque cum odio reiciendis saepe autem numquam quae voluptas molestiae aperiam exercitationem itaque ex repellendus, quis ea provident ut quisquam.",
+    name: "Elon Musk",
+    review: `As an entrepreneur and innovator, I have worked with countless developers over the years, but I must say that working with Raman has been an absolute pleasure. His exceptional skills in coding, design, and user experience have helped us create some of the most innovative and user-friendly products at SpaceX and Tesla.`,
   },
   {
     avatar: AVTR2,
-    name: "Shatta Wale",
-    review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at commodi vel nam doloremque cum odio reiciendis saepe autem numquam quae voluptas molestiae aperiam exercitationem itaque ex repellendus, quis ea provident ut quisquam.",
+    name: "Bill Gates",
+    review: `Not only was Raman’s work on-time and to spec, he pays attention to improve the aesthetics of the site as well as the functionality. Throughout the entire process he is responsive, and willing to work through issues as they arise. It’s obvious he takes tremendous pride in his work, and I wouldn’t hesitate to recommend or work with him again.`,
   },
   {
     avatar: AVTR3,
-    name: "Kwame Despite",
+    name: "Tim Cook",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at commodi vel nam doloremque cum odio reiciendis saepe autem numquam quae voluptas molestiae aperiam exercitationem itaque ex repellendus, quis ea provident ut quisquam.",
+      "Working with Raman was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and steady approach. Time after time, and without us asking, he added enhancements and improvements that resulted in a better end product for us and our clients.",
   },
   {
     avatar: AVTR4,
-    name: "Nana Ama McBrown",
+    name: "Mark Zuckerberg",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod at commodi vel nam doloremque cum odio reiciendis saepe autem numquam quae voluptas molestiae aperiam exercitationem itaque ex repellendus, quis ea provident ut quisquam.",
+      "We have the fortune of working with Raman on a very regular basis. Every single project is done in a timely fashion and is delivered exactly as is outlined. Beyond that, his development skills and expertise make him an invaluable part of our process. We absolutely offer our highest possible recommendation.",
   },
 ];
 
