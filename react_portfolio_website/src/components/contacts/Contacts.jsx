bccc85ccd1ad88dbd838c6209377a1ad8dc0d792
Raman Davidovich <raman.davidovich@gmail.com>
2023-03-12 10:57:24 +0300

    refactor: changed contact options and icons

--- react_portfolio_website/src/components/contacts/Contacts.jsx
+++ react_portfolio_website/src/components/contacts/Contacts.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from "react";
 import "./contacts.css";
 import { MdOutlineEmail } from "react-icons/md";
-import { RiMessengerLine } from "react-icons/ri";
-import { BsWhatsapp } from "react-icons/bs";
+import { FaTelegramPlane } from "react-icons/fa";
+import { SlCallOut } from "react-icons/sl";
 import emailjs from "emailjs-com";
 
 const Contacts = () => {
@@ -24,34 +24,29 @@ const Contacts = () => {
       <div className="container contacts__container">
         <div className="contacts__options">
           <article className="contacts__option">
-            <MdOutlineEmail classname="contacts__option-icon" />
+            <MdOutlineEmail className="contacts__option-icon" />
             <h4>Email</h4>
             <h5>raman.davidovich@gmail.com</h5>
             <a href="mailto:raman.davidovich@gmail.com" target="_blank">
               Send a message
             </a>
           </article>
-          <div className="contacts__options">
-            <article className="contacts__option">
-              <RiMessengerLine className="contacts__option-icon" />
-              <h4>Messenger</h4>
-              <h5>Raman Davidovich</h5>
-              <a href="https://m.me/roman.davidovich.9" target="_blank">
-                Send a message
-              </a>
-            </article>
-            <article className="contacts__option">
-              <BsWhatsapp className="contacts__option-icon" />
-              <h4>WhatsApp</h4>
-              <h5>+123456789</h5>
-              <a
-                href="https://api.whatsapp.com/send?phone=+123456789"
-                target="_blank"
-              >
-                Send a message
-              </a>
-            </article>
-          </div>
+          <article className="contacts__option">
+            <FaTelegramPlane className="contacts__option-icon" />
+            <h4>Telegram</h4>
+            <h5>Raman Davidovich</h5>
+            <a href="https://telegram.me/Raman_Davidovich" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contacts__option">
+            <SlCallOut className="contacts__option-icon" />
+            <h4>Phone Call</h4>
+            <h5>+375(29) 328-47-97</h5>
+            <a href="tel:+375293284797" target="_blank">
+              Make a call
+            </a>
+          </article>
           {/* END OF CONTACT OPTIONS */}
         </div>
         <form ref={form} onSubmit={sendEmail}>
