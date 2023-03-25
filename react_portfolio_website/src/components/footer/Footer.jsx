1e073ef93375f6a633199e208e105e37f030e1f3
Raman Davidovich <raman.davidovich@gmail.com>
2023-03-25 13:37:58 +0300

    feat: added tooltips in Footer

--- react_portfolio_website/src/components/footer/Footer.jsx
+++ react_portfolio_website/src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import "./footer.css";
 import { FaFacebookF } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
 import { IoLogoTwitter } from "react-icons/io";
+import { Tooltip } from "react-tooltip";
 
 const Footer = () => {
   return (
@@ -36,15 +37,37 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com" target="_blank">
+        <a
+          href="https://www.facebook.com/roman.davidovich.9/"
+          target="_blank"
+          className="tooltip_footer"
+          data-tooltip-content="Facebook"
+          data-tooltip-delay-show={500}
+          data-tooltip-variant="dark"
+        >
           <FaFacebookF alt="Facebook icon" />
         </a>
-        <a href="https://instagram.com" target="_blank">
+        <a
+          href="https://www.instagram.com/gryzzlycruise/"
+          target="_blank"
+          className="tooltip_footer"
+          data-tooltip-content="Instagram"
+          data-tooltip-delay-show={500}
+          data-tooltip-variant="dark"
+        >
           <FiInstagram alt="Instagram icon" />
         </a>
-        <a href="https://twitter.com" target="_blank">
+        <a
+          href="https://twitter.com/RoDavidovich"
+          target="_blank"
+          className="tooltip_footer"
+          data-tooltip-content="Twitter"
+          data-tooltip-delay-show={500}
+          data-tooltip-variant="dark"
+        >
           <IoLogoTwitter alt="Twitter icon" />
         </a>
+        <Tooltip anchorSelect=".tooltip_footer" />
       </div>
 
       <div className="footer__copyright">
