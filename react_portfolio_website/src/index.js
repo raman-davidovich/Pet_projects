2f874e617f50b4719c673abfe628bcdd7485496e
Raman Davidovich <raman.davidovich@gmail.com>
2023-03-29 19:41:17 +0300

    refactor:changed way to root render in index.js

--- react_portfolio_website/src/index.js
+++ react_portfolio_website/src/index.js
@@ -1,6 +1,7 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "react-tooltip/dist/react-tooltip.css";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
