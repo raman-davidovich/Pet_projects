cdaa8c18ee44a613c1e4e5780a6e8421664cc32c
Raman Davidovich <raman.davidovich@gmail.com>
2023-04-29 17:46:37 +0300

    refactor: added animation in Services component

--- react_portfolio_website/src/components/services/Services.jsx
+++ react_portfolio_website/src/components/services/Services.jsx
@@ -1,164 +1,52 @@
-import React from "react";
-import "./services.css";
-import { BiCheck } from "react-icons/bi";
+import React, { useContext, useEffect } from "react";
+import "./services.scss";
+import SectionTitle from "../shared/SectionTitle/SectionTitle";
+import { servicesData } from "./constants";
+import { useInView } from "react-intersection-observer";
+import { InViewIDContext } from "../../context/InViewIDContext";
+import { Fade } from "react-awesome-reveal";
+import List from "../shared/List/List";
+import useWindowSize from "@rooks/use-window-size";
+import { getThreshold } from "../shared/utils";
 
 const Services = () => {
+  const { innerHeight, innerWidth } = useWindowSize();
+  const { changeInViewID } = useContext(InViewIDContext);
+
+  const { ref, inView } = useInView({
+    threshold: getThreshold(innerHeight, innerWidth),
+  });
+
+  useEffect(
+    () => changeInViewID(inView ? "services" : ""),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [inView]
+  );
+
   return (
-    <section id="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+    <section id="services" ref={ref}>
+      <SectionTitle>
+        <Fade delay={500} direction="down" cascade triggerOnce damping={0.3}>
+          <span>What I Offer</span>
+          <br />
+          <div>Services</div>
+        </Fade>
+      </SectionTitle>
 
       <div className="container services__container">
-        <article className="service">
-          <div className="service__head">
-            <h3>UI/UX</h3>
-          </div>
+        <Fade delay={1500} direction="up" cascade triggerOnce damping={0.3}>
+          {servicesData.map(({ sectionTitle, list }) => {
+            return (
+              <div className="service" key={sectionTitle}>
+                <div className="service__head">
+                  <h3>{sectionTitle}</h3>
+                </div>
 
-          <ul className="service__list">
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Users and product research.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>
-                Setting up information architecture to identify buyer persona.
-              </p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Creating wireframes and user flows.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Creating prototypes and improve them with new data.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Handling visual design.</p>
-            </li>
-          </ul>
-        </article>
-        {/* END OF UI/UX */}
-        <article className="service">
-          <div className="service__head">
-            <h3>Web Development</h3>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>
-                Selection and registration of a domain and hosting for the site.
-              </p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Website development.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Refinement and updating of websites.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Crossbrowser compatibility.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Refactoring and optimization of current functionality.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Image editing.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Accessibility.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Search engine optimisation.</p>
-            </li>
-          </ul>
-        </article>
-        {/* END OF WEB DEVELOPMENT */}
-        <article className="service">
-          <div className="service__head">
-            <h3>Content Creation</h3>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Research.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Placement.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Publication.</p>
-            </li>
-            <li>
-              <BiCheck
-                className="service__list-icon"
-                alt="Styled list item marker"
-              />
-              <p>Promotion.</p>
-            </li>
-          </ul>
-        </article>
-        {/* END OF CONTENT CREATION */}
+                <List list={list}></List>
+              </div>
+            );
+          })}
+        </Fade>
       </div>
     </section>
   );
