fdf1211194bd7a1fa6f149a4475c9ccf624a5791
Raman Davidovich <raman.davidovich@gmail.com>
2023-04-29 17:44:26 +0300

    refactor: added animation in Footer component

--- react_portfolio_website/src/components/footer/Footer.jsx
+++ react_portfolio_website/src/components/footer/Footer.jsx
@@ -1,78 +1,63 @@
 import React from "react";
-import "./footer.css";
-import { FaFacebookF } from "react-icons/fa";
-import { FiInstagram } from "react-icons/fi";
-import { IoLogoTwitter } from "react-icons/io";
+import "./footer.scss";
 import { Tooltip } from "react-tooltip";
+import footerData from "./constants";
+import { Fade } from "react-awesome-reveal";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">
-        Raman Davidovich
-      </a>
+      <Fade delay={500} direction="up" triggerOnce>
+        <a href="/#" className="footer__logo">
+          Raman Davidovich
+        </a>
+      </Fade>
 
-      <ul className="permalinks">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#testimonials">Testimonials</a>
-        </li>
-        <li>
-          <a href="#contacts">Contacts</a>
-        </li>
+      <nav>
+        <ul className="permalinks">
+          <Fade delay={500} direction="right" cascade triggerOnce damping={0.3}>
+            {footerData.info.map(({ href, content }) => {
+              return (
+                <li key={content}>
+                  <a href={href} aria-label={`Go to ${content} section.`}>
+                    {content}
+                  </a>
+                </li>
+              );
+            })}
+          </Fade>
+        </ul>
+      </nav>
+
+      <ul className="footer__socials">
+        <Fade delay={500} direction="up" cascade triggerOnce damping={0.5}>
+          {footerData.socials.map(({ href, dataTooltipContent, icon }) => {
+            return (
+              <li key={dataTooltipContent}>
+                <a
+                  href={href}
+                  aria-label={`Visit Raman's ${dataTooltipContent} account.`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="tooltip_footer"
+                  data-tooltip-content={dataTooltipContent}
+                  data-tooltip-delay-show={500}
+                  data-tooltip-variant="dark"
+                >
+                  {icon}
+                </a>
+              </li>
+            );
+          })}
+        </Fade>
       </ul>
+      <Tooltip anchorSelect=".tooltip_footer" />
 
-      <div className="footer__socials">
-        <a
-          href="https://www.facebook.com/roman.davidovich.9/"
-          target="_blank"
-          className="tooltip_footer"
-          data-tooltip-content="Facebook"
-          data-tooltip-delay-show={500}
-          data-tooltip-variant="dark"
-        >
-          <FaFacebookF alt="Facebook icon" />
-        </a>
-        <a
-          href="https://www.instagram.com/gryzzlycruise/"
-          target="_blank"
-          className="tooltip_footer"
-          data-tooltip-content="Instagram"
-          data-tooltip-delay-show={500}
-          data-tooltip-variant="dark"
-        >
-          <FiInstagram alt="Instagram icon" />
-        </a>
-        <a
-          href="https://twitter.com/RoDavidovich"
-          target="_blank"
-          className="tooltip_footer"
-          data-tooltip-content="Twitter"
-          data-tooltip-delay-show={500}
-          data-tooltip-variant="dark"
-        >
-          <IoLogoTwitter alt="Twitter icon" />
-        </a>
-        <Tooltip anchorSelect=".tooltip_footer" />
-      </div>
-
-      <div className="footer__copyright">
-        <small>{new Date().getFullYear()} &copy; All rights reserved.</small>
-      </div>
+      <Fade delay={1000} direction="up" triggerOnce>
+        <div className="footer__copyright">
+          <small>{new Date().getFullYear()} &copy; All rights reserved.</small>
+        </div>
+      </Fade>
     </footer>
   );
 };
