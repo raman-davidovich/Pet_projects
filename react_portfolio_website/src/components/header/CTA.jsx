9858a3ed67b736691b5219592576fa335949c2fb
Raman Davidovich <raman.davidovich@gmail.com>
2023-04-29 17:44:36 +0300

    refactor: added animation in CTA component

--- react_portfolio_website/src/components/header/CTA.jsx
+++ react_portfolio_website/src/components/header/CTA.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import CV from "../../assets/CV.pdf";
+import { Fade } from "react-awesome-reveal";
+import Button from "../shared/Button/Button";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
-        Download CV
-      </a>
-      <a href="#contacts" className="btn btn-primary">
-        Let's Talk
-      </a>
+      <Fade delay={2000} direction="right" cascade triggerOnce>
+        <Button tag="a" href={CV} download>
+          Download CV
+        </Button>
+
+        <Button primary tag="a" href="#contacts">
+          Let's Talk
+        </Button>
+      </Fade>
     </div>
   );
 };
