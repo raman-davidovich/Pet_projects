51c9ae72019e25a59d2a11500fc9b7abf2b6ce68
Raman Davidovich <raman.davidovich@gmail.com>
2023-04-29 17:46:19 +0300

    refactor: added animation in ProgressBar component

--- react_portfolio_website/src/components/progressBar/ProgressBar.jsx
+++ react_portfolio_website/src/components/progressBar/ProgressBar.jsx
@@ -1,16 +1,8 @@
 import React, { useEffect } from "react";
-import "./progressBar.css";
+import "./progressBar.scss";
+import { onScroll } from "./utils";
 
 const ProgressBar = () => {
-  const onScroll = () => {
-    let pixelsFromTop = window.scrollY;
-    let documentHeight = document.body.clientHeight;
-    let windowHeight = window.innerHeight;
-    let difference = documentHeight - windowHeight;
-    let percentage = (100 * pixelsFromTop) / difference;
-    document.querySelector(".progress_bar").style.width = `${percentage}%`;
-  };
-
   useEffect(() => {
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
