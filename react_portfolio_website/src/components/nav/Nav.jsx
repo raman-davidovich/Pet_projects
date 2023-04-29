7d69fcfa876faec544eb50ea4ddeba3db02037ea
Raman Davidovich <raman.davidovich@gmail.com>
2023-04-29 17:45:42 +0300

    refactor: added animation in Nav component

--- react_portfolio_website/src/components/nav/Nav.jsx
+++ react_portfolio_website/src/components/nav/Nav.jsx
@@ -1,76 +1,51 @@
-import React, { useState } from "react";
-import "./nav.css";
-import { AiOutlineHome } from "react-icons/ai";
-import { AiOutlineUser } from "react-icons/ai";
-import { BiBook } from "react-icons/bi";
-import { RiServiceLine } from "react-icons/ri";
-import { BiMessageSquareDetail } from "react-icons/bi";
+import React, { useContext, useEffect, useState } from "react";
+import "./nav.scss";
+import navData from "./constants";
 import { Tooltip } from "react-tooltip";
+import { InViewIDContext } from "../../context/InViewIDContext";
+import classNames from "classnames";
+import useDebounce from "@rooks/use-debounce";
+import { Fade } from "react-awesome-reveal";
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState("#");
+  const { inViewSectionID } = useContext(InViewIDContext);
+
+  const [activeNav, setActiveNav] = useState(inViewSectionID);
+
+  const setDebouncedNav = useDebounce(setActiveNav, 400);
+
+  useEffect(() => {
+    setDebouncedNav(inViewSectionID);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inViewSectionID]);
+
   return (
-    <nav>
-      <a
-        href="#"
-        onClick={() => setActiveNav("#")}
-        className={activeNav === "#" ? "active tooltip_nav" : "tooltip_nav"}
-        data-tooltip-content="Home"
-        data-tooltip-delay-show={500}
-        data-tooltip-variant="info"
-      >
-        <AiOutlineHome alt="Home section icon" />
-      </a>
-      <a
-        href="#about"
-        onClick={() => setActiveNav("#about")}
-        className={
-          activeNav === "#about" ? "active tooltip_nav" : "tooltip_nav"
-        }
-        data-tooltip-content="About me"
-        data-tooltip-delay-show={500}
-        data-tooltip-variant="info"
-      >
-        <AiOutlineUser alt="About section icon" />
-      </a>
-      <a
-        href="#experience"
-        onClick={() => setActiveNav("#experience")}
-        className={
-          activeNav === "#experience" ? "active tooltip_nav" : "tooltip_nav"
-        }
-        data-tooltip-content="Experience"
-        data-tooltip-delay-show={500}
-        data-tooltip-variant="info"
-      >
-        <BiBook alt="Experience section icon" />
-      </a>
-      <a
-        href="#services"
-        onClick={() => setActiveNav("#services")}
-        className={
-          activeNav === "#services" ? "active tooltip_nav" : "tooltip_nav"
-        }
-        data-tooltip-content="Services"
-        data-tooltip-delay-show={500}
-        data-tooltip-variant="info"
-      >
-        <RiServiceLine alt="Services section icon" />
-      </a>
-      <a
-        href="#contacts"
-        onClick={() => setActiveNav("#contacts")}
-        className={
-          activeNav === "#contacts" ? "active tooltip_nav" : "tooltip_nav"
-        }
-        data-tooltip-content="Contacts"
-        data-tooltip-delay-show={500}
-        data-tooltip-variant="info"
-      >
-        <BiMessageSquareDetail alt="Contacts section icon" />
-      </a>
-      <Tooltip anchorSelect=".tooltip_nav" />
-    </nav>
+    <Fade delay={4000} direction="up" triggerOnce className="navigation">
+      <nav>
+        <Fade delay={4500} direction="up" triggerOnce cascade damping={0.3}>
+          {navData.map(({ href, className, dataTooltipContent, icon }) => {
+            return (
+              <a
+                key={className}
+                href={href}
+                aria-label={`Go to section ${dataTooltipContent}`}
+                onClick={() => setActiveNav({ className })}
+                className={classNames("tooltip_nav", {
+                  active: activeNav === className,
+                })}
+                data-tooltip-content={dataTooltipContent}
+                data-tooltip-delay-show={500}
+                data-tooltip-variant="info"
+              >
+                {icon}
+              </a>
+            );
+          })}
+        </Fade>
+
+        <Tooltip anchorSelect=".tooltip_nav" />
+      </nav>
+    </Fade>
   );
 };
 
