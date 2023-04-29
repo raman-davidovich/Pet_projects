a7c6758d8558201dd2354dfbd9202b87fcc312d9
Raman Davidovich <raman.davidovich@gmail.com>
2023-04-29 17:44:15 +0300

    refactor: added animation in Experience component

--- react_portfolio_website/src/components/experience/Experience.jsx
+++ react_portfolio_website/src/components/experience/Experience.jsx
@@ -1,115 +1,73 @@
-import React from "react";
-import "./experience.css";
+import React, { useContext, useEffect } from "react";
+import "./experience.scss";
+import experienceData from "./constants";
+import SectionTitle from "../shared/SectionTitle/SectionTitle";
 import { BsPatchCheckFill } from "react-icons/bs";
+import { useInView } from "react-intersection-observer";
+import { InViewIDContext } from "../../context/InViewIDContext";
+import { Fade } from "react-awesome-reveal";
+import useWindowSize from "@rooks/use-window-size";
+import { getThreshold } from "../shared/utils";
 
 const Experience = () => {
+  const { innerHeight, innerWidth } = useWindowSize();
+  const { changeInViewID } = useContext(InViewIDContext);
+
+  const { ref, inView } = useInView({
+    threshold: getThreshold(innerHeight, innerWidth),
+  });
+
+  useEffect(
+    () => changeInViewID(inView ? "experience" : ""),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [inView]
+  );
+
   return (
-    <section id="experience">
-      <h5>The Skills I Have</h5>
-      <h2>My Experience</h2>
+    <section id="experience" ref={ref}>
+      <SectionTitle>
+        <Fade delay={500} direction="down" cascade triggerOnce damping={0.3}>
+          <span>The Skills I Have</span>
+          <br />
+          <div>My Experience</div>
+        </Fade>
+      </SectionTitle>
 
       <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
+        <Fade delay={1500} direction="up" cascade triggerOnce damping={0.5}>
+          {experienceData.map(({ title, list }) => {
+            return (
+              <div key={title}>
+                <h3>{title}</h3>
+
+                <ul className="experience__content">
+                  <Fade
+                    delay={2000}
+                    direction="up"
+                    triggerOnce
+                    cascade
+                    damping={0.2}
+                  >
+                    {list.map(({ technology, level }) => {
+                      return (
+                        <li key={technology} className="experience__details">
+                          <BsPatchCheckFill
+                            className="experience__details-icon"
+                            alt="Styled list item marker"
+                          />
+                          <div>
+                            <h4>{technology}</h4>
+                            <small className="text-light">{level}</small>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </Fade>
+                </ul>
               </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>CSS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>JavaScript</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>TypeScript</h4>
-                <small className="text-light">Basic</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>Tailwind</h4>
-                <small className="text-light">Basic</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>ReactJS</h4>
-                <small className="text-light">Basic</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>Figma</h4>
-                <small className="text-light">Basic</small>
-              </div>
-            </article>
-          </div>
-        </div>
-        {/* END OF FRONTEND */}
-        <div className="experience__backend">
-          <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>Node JS</h4>
-                <small className="text-light">Basic</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill
-                className="experience__details-icon"
-                alt="Styled list item marker"
-              />
-              <div>
-                <h4>MySQL</h4>
-                <small className="text-light">Basic</small>
-              </div>
-            </article>
-          </div>
-        </div>
+            );
+          })}
+        </Fade>
       </div>
     </section>
   );
